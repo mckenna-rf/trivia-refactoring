f4d35ff2418b4a91395dc8b99a2f5345e70bfd0f
McKenna Fellows <mckennafellows@gmail.com>
2021-10-01 23:54:12 -0500

    adding test code for trivia.py methods

--- triviaTest.py
+++ triviaTest.py
@@ -1,9 +1,27 @@
 from trivia import Game
 
 
-class TriviaTest:
+class TriviaTest: # a few tests for specific methods in trivia.py
 
     def test_trivia(self):
         game = Game()
         game.add('Chet')
         assert len(game.players) == 2
+    
+    def gameIsPlayable(self):
+        game = Game()
+        game.add('Pat')
+        game.add('Sue')
+        assert self.is_playable() == True
+
+    def gameIsNotPlayable(self):
+        game = Game()
+        game.add('Pat')
+        assert self.is_playable() == False
+    
+    def testCreateQ(self):
+        game = Game()
+        q = game.create_rock_question("Rock?")
+        assert q == "Rock Question"
+
+    
